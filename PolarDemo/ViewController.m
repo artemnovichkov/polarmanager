07b8d4d6c37a9eeff43ea3aaed32195d183a24de
Artem Novichkov <artem.novichkov@rosberry.com>
2016-02-25 11:12:49 +0600

    ViewController has been updated with delegate for manager and peripheral

--- PolarDemo/ViewController.m
+++ PolarDemo/ViewController.m
@@ -7,7 +7,6 @@
 //
 
 #import "ViewController.h"
-@import CoreBluetooth;
 
 static NSString *const kDeviceInformationUUID = @"180A";
 static NSString *const kHeartRateServiceUUID = @"180D";
@@ -27,6 +26,69 @@ static NSString *const kManufacturerNameCharacteristicUUID = @"2A29";
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.centralManager = [[CBCentralManager alloc] initWithDelegate:self queue:nil];
+}
+
+#pragma mark - CBCentralManagerDelegate
+
+- (void)centralManager:(CBCentralManager *)central didConnectPeripheral:(CBPeripheral *)peripheral {
+    
+}
+
+- (void)centralManager:(CBCentralManager *)central didDiscoverPeripheral:(CBPeripheral *)peripheral advertisementData:(NSDictionary<NSString *, id> *)advertisementData RSSI:(NSNumber *)RSSI {
+    
+}
+
+- (void)centralManagerDidUpdateState:(CBCentralManager *)central {
+    switch (central.state) {
+        case CBCentralManagerStatePoweredOff:
+            NSLog(@"CoreBluetooth BLE hardware is powered off");
+            break;
+        case CBCentralManagerStatePoweredOn: {
+            NSLog(@"CoreBluetooth BLE hardware is powered on and ready");
+//            NSArray<CBUUID *> *services = @[[CBUUID UUIDWithString:kHeartRateServiceUUID], [CBUUID UUIDWithString:kDeviceInformationUUID]];
+            [self.centralManager scanForPeripheralsWithServices:nil options:nil];
+            break;
+        }
+        case CBCentralManagerStateUnauthorized:
+            NSLog(@"CoreBluetooth BLE state is unauthorized");
+            break;
+        case CBCentralManagerStateUnknown:
+            NSLog(@"CoreBluetooth BLE state is unknown");
+            break;
+        case CBCentralManagerStateUnsupported:
+            NSLog(@"CoreBluetooth BLE hardware is unsupported on this platform");
+            break;
+        default:
+            break;
+    }
+}
+
+#pragma mark - CBPeripheralDelegate
+
+- (void)peripheral:(CBPeripheral *)peripheral didDiscoverServices:(nullable NSError *)error {
+    
+}
+
+- (void)peripheral:(CBPeripheral *)peripheral didDiscoverCharacteristicsForService:(CBService *)service error:(nullable NSError *)error {
+    
+}
+
+- (void)peripheral:(CBPeripheral *)peripheral didUpdateValueForCharacteristic:(CBCharacteristic *)characteristic error:(nullable NSError *)error {
+    
+}
+
+#pragma mark - CBCharacteristic Helpers
+
+- (void)getHeartBPMData:(CBCharacteristic *)characteristic error:(NSError *)error {
+    
+}
+
+- (void)getManufacturerName:(CBCharacteristic *)characteristic {
+    
+}
+
+- (void)getBodyLocation:(CBCharacteristic *)characteristic {
     
 }
 
