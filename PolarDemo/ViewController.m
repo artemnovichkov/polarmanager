2725c4128ece4b2a33120c0704539feec46d4596
Artem Novichkov <artem.novichkov@rosberry.com>
2016-02-25 11:39:46 +0600

    ViewController has been updated with getting of bpm

--- PolarDemo/ViewController.m
+++ PolarDemo/ViewController.m
@@ -32,11 +32,21 @@ static NSString *const kManufacturerNameCharacteristicUUID = @"2A29";
 #pragma mark - CBCentralManagerDelegate
 
 - (void)centralManager:(CBCentralManager *)central didConnectPeripheral:(CBPeripheral *)peripheral {
-    
+    peripheral.delegate = self;
+    [peripheral discoverServices:nil];
+    self.connected = [NSString stringWithFormat:@"Connected: %@", peripheral.state == CBPeripheralStateConnected ? @"YES" : @"NO"];
+    NSLog(@"%@", self.connected);
 }
 
 - (void)centralManager:(CBCentralManager *)central didDiscoverPeripheral:(CBPeripheral *)peripheral advertisementData:(NSDictionary<NSString *, id> *)advertisementData RSSI:(NSNumber *)RSSI {
-    
+    NSString *localName = advertisementData[CBAdvertisementDataLocalNameKey];
+    if (localName.length > 0) {
+        NSLog(@"Found the heart rate monitor: %@", localName);
+        [self.centralManager stopScan];
+        self.polarPeripheral = peripheral;
+        peripheral.delegate = self;
+        [self.centralManager connectPeripheral:peripheral options:nil];
+    }
 }
 
 - (void)centralManagerDidUpdateState:(CBCentralManager *)central {
@@ -67,21 +77,60 @@ static NSString *const kManufacturerNameCharacteristicUUID = @"2A29";
 #pragma mark - CBPeripheralDelegate
 
 - (void)peripheral:(CBPeripheral *)peripheral didDiscoverServices:(nullable NSError *)error {
-    
+    for (CBService *service in peripheral.services) {
+        NSLog(@"Discovered service: %@", service.UUID);
+        [peripheral discoverCharacteristics:nil forService:service];
+    }
 }
 
 - (void)peripheral:(CBPeripheral *)peripheral didDiscoverCharacteristicsForService:(CBService *)service error:(nullable NSError *)error {
-    
+    if ([service.UUID isEqual:[CBUUID UUIDWithString:kHeartRateServiceUUID]]) {
+        for (CBCharacteristic *characteristic in service.characteristics) {
+            if ([characteristic.UUID isEqual:[CBUUID UUIDWithString:kMeasurementCharacteristicUUID]]) {
+                [self.polarPeripheral setNotifyValue:YES forCharacteristic:characteristic];
+                NSLog(@"Found heart rate measurement characteristic");
+            } else if ([characteristic.UUID isEqual:[CBUUID UUIDWithString:kBodyLocationCharacteristicUUID]]) {
+                [self.polarPeripheral readValueForCharacteristic:characteristic];
+                NSLog(@"Found body sensor location characteristic");
+            }
+        }
+    } else if ([service.UUID isEqual:[CBUUID UUIDWithString:kDeviceInformationUUID]]) {
+        for (CBCharacteristic *characteristic in service.characteristics) {
+            if ([characteristic.UUID isEqual:[CBUUID UUIDWithString:kManufacturerNameCharacteristicUUID]]) {
+                [self.polarPeripheral readValueForCharacteristic:characteristic];
+                NSLog(@"Found a device manufacturer name characteristic");
+            }
+        }
+    }
 }
 
 - (void)peripheral:(CBPeripheral *)peripheral didUpdateValueForCharacteristic:(CBCharacteristic *)characteristic error:(nullable NSError *)error {
-    
+    if ([characteristic.UUID isEqual:[CBUUID UUIDWithString:kMeasurementCharacteristicUUID]]) {
+        [self getHeartBPMData:characteristic error:error];
+    }
+    if ([characteristic.UUID isEqual:[CBUUID UUIDWithString:kManufacturerNameCharacteristicUUID]]) {
+        [self getManufacturerName:characteristic];
+    }
+    if ([characteristic.UUID isEqual:[CBUUID UUIDWithString:kBodyLocationCharacteristicUUID]]) {
+        [self getBodyLocation:characteristic];
+    }
 }
 
 #pragma mark - CBCharacteristic Helpers
 
 - (void)getHeartBPMData:(CBCharacteristic *)characteristic error:(NSError *)error {
+    NSData *data = characteristic.value;
+    const uint8_t *reportData = data.bytes;
+    uint16_t bpm = 0;
     
+    if ((reportData[0] & 0x01) == 0) {
+        bpm = reportData[1];
+    } else {
+        bpm = CFSwapInt16LittleToHost(*(uint16_t *)(&reportData[1]));
+    }
+    if (characteristic.value || !error) {
+        self.label.text = [NSString stringWithFormat:@"%hu bpm", bpm];
+    }
 }
 
 - (void)getManufacturerName:(CBCharacteristic *)characteristic {
